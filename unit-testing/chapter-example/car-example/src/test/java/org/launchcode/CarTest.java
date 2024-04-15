8bbdf1ecc49f44175a6e15fc587d2c6a0cc5dd92
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-04-15 11:10:34 -0500

    Class 5 - Unit Testing - 2

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -14,6 +14,7 @@ class CarTest {
     }
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+
 //    @Test
 //    public void emptyTest() {
 //        assertEquals(10,10,.001);
@@ -44,14 +45,27 @@ class CarTest {
 
     //TODO: gasTankLevel is accurate after driving within tank range
 
-    
+    @Test
+    public void testGasTankAfterDriving(){
+    test_car.drive(50);
+      assertEquals(9, test_car.getGasTankLevel(),.001);
+    }
 
 
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
 
+    @Test
+    public void testGasTanAfterDriving() {
+    test_car.drive(1000);
+    assertEquals(test_car.getGasTankLevel(), 0, .001);
+    }
 
     //TODO: can't have more gas than tank size, expect an exception
 
-
+    @Test()
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5),"Shouldn't get here, car cannot " +
+                "have more gas in tank than the size of the tank");
+    }
 
 }
